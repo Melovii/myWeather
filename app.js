3a0c0607ad576a56d82483a580ff4f58c6b320c7
Melovii <145908254+Melovii@users.noreply.github.com>
2024-10-01 17:53:19 +0300

    finished implementation to fetch weatherData and made a function for displaying it

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const API_KEYS = {
-    WEATHER: 'INSERT WEATHER API KEY HERE',
+    WEATHER: 'X3WL6UFP39QRKSNKS4JDBEYWS',
     NEWS: 'INSERT NEWS API KEY HERE',
     GIFS: 'INSERT GIF API KEY HERE',
     UNSPLASH: 'INSERT UNSPLASH API KEY HERE',
@@ -9,15 +9,52 @@ async function fetchWeather(location) {
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEYS.WEATHER}`);
         const data = await response.json();
-        return data;
+        return processWeatherData(data);
     } catch (error) {
         console.log('Error fetching weather data:', error);
     }
 }
 
+function processWeatherData(data) {
+    if (!data || !data.currentConditions) {
+        return null;
+    }
+
+    return {
+        conditions: data.currentConditions.conditions,
+        feelslike: data.currentConditions.feelslike,
+        sunrise: data.currentConditions.sunrise,
+        sunset: data.currentConditions.sunset,
+        humidity: data.currentConditions.humidity,
+        windspeed: data.currentConditions.windspeed,
+        temp: data.currentConditions.temp,
+        address: data.resolvedAddress,
+    };
+}
+
+function displayWeatherData(data) {
+    const weatherInfoDiv = document.getElementById('weather-info');
+    weatherInfoDiv.innerHTML = `
+    <h2>${data.address}</h2>
+    <p>Conditions: ${data.conditions}</p>
+    <p>Feels like: ${data.feelslike}°C</p>
+    <p>Humidity: ${data.humidity}%</p>
+    <p>Wind Speed: ${data.windspeed} kmh</p>
+    <p>Temperature: ${data.temp}°C</p>
+    <p>Sunrise: ${data.sunrise}</p>
+    <p>Sunset: ${data.sunset}</p>
+    `;
+}
+
 document.getElementById('location-form').addEventListener('submit', async (event) => {
     event.preventDefault();
     const location = document.getElementById('location-input').value;
+
     const weatherData = await fetchWeather(location);
-    console.log(weatherData);
+    if (weatherData) {
+        console.log(weatherData);
+        displayWeatherData(weatherData);
+    } else {
+        console.log('Error fetching weather data');
+    }
 });
